142652539d37e81b01b81e0bdde34993174e0ff2
suggee <wangchenhao123@126.com>
2016-07-04 17:06:49 +0800

    update the lazy fragment

--- app/src/main/java/com/suggee/edustudent/base/ui/BaseLazyFragment.java
+++ app/src/main/java/com/suggee/edustudent/base/ui/BaseLazyFragment.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
  * Email:   wangchenghao@howdo.cc | wangchenghao123@126.com
  * Date:    16/6/23
  * Description:
- *             实现fragment懒加载，主要用于viewpager的加载
+ *             实现fragment懒加载，主要用于viewpager的加载，而不是采用FragmentManager来进行show/hide的操作
  */
 public abstract class BaseLazyFragment extends BaseFragment {
 
